2551dd7bf2470e114830f8df8b349fcd15a1a802
hailong1606 <hailong160697@gmail.com>
2019-03-11 11:04:37 +0700

    Fizz Buzz 2

---
+++ fizz2.js
@@ -0,0 +1,12 @@
+for( var i =1; i<=100;i++)
+{
+    if (i%3==0&&i%5==0)
+    {
+        console.log("Sphinx Software");
+    }
+    else
+    {
+        i%3==0?console.log("Sphinx"):i%5==0?console.log("Software"):console.log(i);
+    }
+    
+}
