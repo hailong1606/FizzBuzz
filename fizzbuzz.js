2442711c50586edbaf53df2aab79360c350efc79
hailong1606 <hailong160697@gmail.com>
2019-03-11 10:10:03 +0700

    FizzBuzz

---
+++ fizzbuzz.js
@@ -0,0 +1,19 @@
+for( var i =1; i<=100;i++)
+{
+    if (i%3==0&&i%5==0)
+    {
+        console.log("Sphinx Software");
+    }
+    else
+    {
+        if(i%3==0||i%5==0)
+        {
+            console.log("Sphinx");
+        }
+        else
+        {
+            console.log(i);
+        }
+    }
+    
+}
